72f9cd7a9e031c0b35adb219076d2900ef9b7996
AngelBrambila91 <malthael.91@gmail.com>
2023-05-25 08:38:05 -0600

    I suffer for you ... Stupid HTML

---
+++ 3P/PracticalApps/Northwind.Web/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+if(!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+// Configure for using Static pages
+app.UseDefaultFiles();
+app.UseStaticFiles();
+// Configure to use Razor Pages
+app.MapRazorPages();
+
+// GET
+app.MapGet("/hello", () => "Hello World!");
+
+app.MapGet("/Raspados", () => "Que ricos Son"); // return "Que ricos son"
+
+app.Run();
