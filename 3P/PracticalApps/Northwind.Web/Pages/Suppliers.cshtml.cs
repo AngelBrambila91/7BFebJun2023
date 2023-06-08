7cf16e8246c583d0f63f6b561df1cf0eecbfb0c0
AngelBrambila91 <malthael.91@gmail.com>
2023-06-08 07:33:08 -0600

    Created Razor Library for Employees

--- 3P/PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
+++ 3P/PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -1,19 +1,36 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc; // RedirectPage("/Suppliers")
 using Northwind.Shared;
 
 namespace Northwind.Web.Pages;
 
 public class SuppliersModel : PageModel
 {
-private NorthwindContext db;
-public SuppliersModel(NorthwindContext injectedContext)
-{
-    db = injectedContext;
-}
-public IEnumerable<Supplier>? Suppliers { get; set; }
-public void OnGet()
-{
-    ViewData["Title"] = "Raspados - Suppliers";
-    Suppliers = db.Suppliers.OrderBy(c => c.Country).ThenBy(c => c.CompanyName);
-}
+    private NorthwindContext db;
+    public SuppliersModel(NorthwindContext injectedContext)
+    {
+        db = injectedContext;
+    }
+    public IEnumerable<Supplier>? Suppliers { get; set; }
+    public void OnGet()
+    {
+        ViewData["Title"] = "Raspados - Suppliers";
+        Suppliers = db.Suppliers.OrderBy(c => c.Country).ThenBy(c => c.CompanyName);
+    }
+
+    [BindProperty]
+    public Supplier? Supplier { get; set; }
+    public IActionResult OnPost()
+    {
+        if ((Supplier is not null) && ModelState.IsValid)
+        {
+            db.Suppliers.Add(Supplier);
+            db.SaveChanges();
+            return RedirectToPage("/suppliers");
+        }
+        else
+        {
+            return Page();
+        }
+    }
 }
